d95002558122c048ed97b72b5b3bee5ebe598602
harbinman <harbinman2012@gmail.com>
2024-12-20 12:02:16 +0800

    modify phone number get method

--- apiCaller.js
+++ apiCaller.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+require('dotenv').config();
 async function callAPI(ipList) {
     if (ipList.length === 0) {
         console.log('所有 IP 都可达，无需调用接口。');
@@ -8,7 +8,7 @@ async function callAPI(ipList) {
 
     const url = process.env.API_URL; // 注意 URL 添加了 `http://`
     const data = JSON.stringify({
-        mobile: process.env.PHONE_NUMBER.split(','), // 测试手机号，可替换为实际需要发送的号码
+        mobile: process.env.PHONE_NUMBER, // 测试手机号，可替换为实际需要发送的号码
         content: `服务器${ipList.join(', ')}已无法访问,请及时处理！`, // 动态内容
         systemName: '统一服务系统',
         type: 1,
